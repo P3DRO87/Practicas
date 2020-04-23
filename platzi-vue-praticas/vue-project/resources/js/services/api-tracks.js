447d23db26285702e0a54ca56d1a2e9ab6927090
antoniofloresonline <wanllyf@gmail.com>
2020-04-23 10:44:59 -0600

    ::

--- platzi-vue-praticas/vue-project/resources/js/services/api-tracks.js
+++
@@ -1,8 +0,0 @@
-import trae from "trae";
-import configServie from "./config";
-
-const musicService = trae.create({
-  baseUrl: configServie.apiUrl,
-});
-
-export default musicService;
--- platzi-vue-praticas/vue-project/resources/js/services/config.js
+++
@@ -1,5 +0,0 @@
-const configServie = {
-  apiUrl: "https://platzi-music-api.now.sh",
-};
-
-export default configServie;
