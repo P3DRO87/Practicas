91cac89f2c1c04bff87804ddb0d7236e0fa0f54b
Pedro <correo@correo.com>
2020-04-24 15:16:58 -0600

    Revert "Larevel"
    This reverts commit 983928fc0c18c8bf01192e0a67b5e1d314ea0bda.

---
+++ resources/js/TasksManagerApp/TasksManagerApp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="tasksManagerApp" class="main container">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <br />
+    <router-link class="btn btn-primary" to="/">Back to Home</router-link>
+    <br />
+    <br />
+    <router-link class="btn btn-primary" to="/single">Change page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TasksManagerApp"
+};
+</script>
+
+<style lang="scss">
+#productsApp {
+  min-height: 100vh;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.5s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ resources/views/tasks-list.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Task List')
+
+@section('content')
+	<div id="tasksManagerApp">
+	
+	</div>
+@endsection
