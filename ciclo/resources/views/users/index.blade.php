91bab25122663801f1d5b5fc0ef242e9346f61a5
Pedro <correo@correo.com>
2020-04-29 00:46:10 -0600

    adding more practice

---
+++ ciclo/resources/views/users/index.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ciclo</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+       <div class="container">
+           <div class="row">
+               <div class="col-sm-8 mx-auto"> 
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach($errors->all() as $error)
+                                        - {{ $error }} <br>
+                                    @endforeach()
+                                </div>
+                            @endif()
+
+                            <form action="{{route('users.store')}}" method="POST">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        <input
+                                        value="{{old('name')}}" 
+                                        type="text" 
+                                        name="name" 
+                                        class="form-control" 
+                                        placeholder="Nombre">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input
+                                        value="{{old('email')}}"  
+                                        type="email" 
+                                        name="email" 
+                                        class="form-control" 
+                                        placeholder="Email">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input 
+                                        type="password" 
+                                        name="password" 
+                                        class="form-control" 
+                                        placeholder="Password">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf <!-- indica que el formulario es nuestro -->
+                                        <button type="submit" class="btn btn-dark">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>    
+                        </div>
+                    </div>
+
+                    <table class="table">
+                       <thead>
+                           <tr>
+                               <th>ID</th>
+                               <th>Nombre</th>
+                               <th>Email</th>
+                               <th>&nbsp;</th>
+                           </tr>
+                       </thead>
+                       <tbody>
+                           @foreach($users as $user)
+                           <tr>
+                               <td>{{$user->id}}</td> 
+                               <td>{{$user->name}}</td>
+                               <td>{{$user ->email}}</td> 
+                               <td>
+                                   <form action="{{route('users.destroy', $user)}}" method="POST">
+                                       @method('DELETE')
+                                       @csrf
+                                       <input
+                                        type="submit"
+                                        value="eliminar"
+                                        class="btn btn-danger"
+                                        onclick="return confirm('remove?')"
+                                        >
+                                   </form>
+                               </td>
+                           </tr>
+                           @endforeach
+                       </tbody>
+                    </table>
+               </div>
+           </div>
+       </div>
+    </body>
+</html>
